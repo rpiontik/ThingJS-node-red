73059b378407f6766d568162271bdcb99b2175e8
Пионтик Роман <rip@rabota.ru>
2020-10-08 21:02:16 +0300

    Refactoring user settings

---
+++ controller.js
@@ -0,0 +1,13 @@
+module.exports = function() {
+    return {
+        getConfig() {
+            return {
+                url: '192.168.8.102'
+            }
+        },
+
+        postConfig() {
+
+        }
+    }
+}

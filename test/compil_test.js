0bd942f6001752175cc79deb54110c11999ae357
Пионтик Роман <rip@rabota.ru>
2020-10-24 17:18:03 +0300

    Refactored function node Added multiwire

--- test/compil_test.js
+++ test/compil_test.js
@@ -1,39 +1,10 @@
 let $$nop = function () {
 };
+let $$fnd = function (v) {
+    return typeof v !== "undefined"
+};
 let $$nd = {
-    "rev": "47252949d88b7a442c7b50208789f2cb", "f2ef097e.ccb1b8": function ($$i) {
-        let $$n = $$nd["da392213.6cda8"];
-        let inject = function () {
-            $$n({"payload": 1});
-        };
-        $$r.timer.setInterval(function () {
-            let tm = $$r.clock.parse();
-            if ((tm.hour >= 2) && (tm.hour <= 4) && (tm.wday === 1) && (tm.wday === 2) && (tm.wday === 3) && (tm.wday === 4) && (tm.wday === 5) && (tm.wday === 0)) {
-                inject();
-            }
-        }, 120000);
-    }, "4e542ce2.dca1b4": function ($$i) {
-        let $$n = function ($$i) {
-            $$nd["915bd838.c69298"]($$i);
-            $$nd["da392213.6cda8"]($$i);
-        };
-        let inject = function () {
-            $$n({
-                "payload": 333,
-                "topic": $$i["payload"],
-                "field1": 123,
-                "field2": true,
-                "field3": {"test": "json"},
-                "field4": "\x74\x65\x73\x74"
-            });
-        };
-        $$r.timer.setInterval(function () {
-            let tm = $$r.clock.parse();
-            if ((tm.hour >= 0) && (tm.hour <= 4)) {
-                inject();
-            }
-        }, 360000);
-    }, "da392213.6cda8": function ($$i) {
+    "rev": "d80dfb9c356bbbb9cfeb6ac3a84cffe5", "da392213.6cda8": function ($$i) {
         let $$n = $$nop;
         print($$i);
     }, "915bd838.c69298": function ($$i) {
@@ -45,7 +16,42 @@ let $$nd = {
     }, "b49235a7.1a8298": function ($$i) {
         let $$n = $$nd["f6ae5432.fadae8"];
         $$n($$i);
+    }, "cab0bd30.540d3": function ($$i) {
+        let $$n = function ($$i) {
+            if ($$fnd($$i[0])) {
+                $$nd["915bd838.c69298"]($$i[0]);
+            }
+            if ($$fnd($$i[2])) {
+                $$nd["89a58e2f.47791"]($$i[2]);
+            }
+        };
+        $$n((function (msg) {
+            return [msg, {"payload": "test2", "field1": "field1"}, {"test": "pest"}];
+        })($$i));
+    }, "89a58e2f.47791": function ($$i) {
+        let $$n = $$nop;
+        print($$i);
     }
 };
-$$nd["f2ef097e.ccb1b8"]();
-$$nd["4e542ce2.dca1b4"]();
+(function () {
+    let $$n = function ($$i) {
+        if ($$fnd($$i)$$nd["da392213.6cda8"]($$i);
+        if ($$fnd($$i)$$nd["cab0bd30.540d3"]($$i);
+    };
+    let inject = function () {
+        $$n({
+            "payload": 333,
+            "topic": $$i["payload"],
+            "field1": 123,
+            "field2": true,
+            "field3": {"test": "json"},
+            "field4": "\x74\x65\x73\x74"
+        });
+    };
+    $$r.timer.setInterval(function () {
+        let tm = $$r.clock.parse();
+        if ((tm.hour >= 0) && (tm.hour <= 4)) {
+            inject();
+        }
+    }, 360000);
+})();
